ed3612422eb3cd8c3ef9cd8fd5b419955e8ace1d
Charles Donizete Alves dos Santos <82051427+CharlesDonizete@users.noreply.github.com>
2024-05-18 10:41:14 -0300

    Criando modelo da tela de listagem

--- src/app/pages/sistema/sistema.component.ts
+++ src/app/pages/sistema/sistema.component.ts
@@ -11,6 +11,46 @@ import { SistemaService } from 'src/app/services/sistema.service';
   styleUrls: ['./sistema.component.scss'],
 })
 export class SistemaComponent {
+  tipoTela: number = 1; //1 listagem, 2 cadastro e 3 edição
+  tableListSistemas: Array<SistemaFinanceiro>;
+  id: string;
+  page: number = 1;
+  config: any;
+  paginacao: boolean = true;
+  itemsPorPagina: number = 10;
+
+  configpag() {
+    this.id = this.gerarIdParaConfigDePaginacao();
+
+    this.config = {
+      id: this.id,
+      currentPage: this.page,
+      itemsPerPage: this.itemsPorPagina,
+    };
+  }
+
+  gerarIdParaConfigDePaginacao() {
+    var result = '';
+    var characters =
+      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for (var i = 0; i < 10; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+  }
+
+  ListaSistemasUsuario() {
+    this.tipoTela = 1;
+
+    this.sistemaService
+      .ListaSistemaUsuario(this.authService.getEmailUser())
+      .subscribe((response: Array<SistemaFinanceiro>) => {
+        this.tableListSistemas = response;
+      }),
+      (error) => console.error(error);
+  }
+
   constructor(
     public menuService: MenuService,
     public formBuilder: FormBuilder,
@@ -22,6 +62,9 @@ export class SistemaComponent {
 
   ngOnInit() {
     this.menuService.menuSelecionado = 2;
+
+    this.configpag();
+    this.ListaSistemasUsuario();
 
     this.sistemaForm = this.formBuilder.group({
       name: ['', [Validators.required]],
@@ -63,5 +106,25 @@ export class SistemaComponent {
           (error) => console.error(error);
       }),
       (error) => console.error(error);
+  }
+
+  cadastro() {
+    this.tipoTela = 2;
+    this.sistemaForm.reset();
+  }
+
+  // edicao(id:number){
+
+  // }
+
+  mudarPage(event: any) {
+    this.page = event;
+    this.config.currentPage = this.page;
+  }
+
+  mudarItemsPorPage() {
+    this.page = 1;
+    this.config.currentPage = this.page;
+    this.config.itemsPerPage = this.itemsPorPagina;
   }
 }
--- src/app/pages/sistema/sistema.module.ts
+++ src/app/pages/sistema/sistema.module.ts
@@ -5,7 +5,10 @@ import { SistemaRoutingModule } from './sistema-routing.module';
 import { SistemaComponent } from './sistema.component';
 import { NavbarModule } from 'src/app/components/navbar/navbar.module';
 import { SidebarModule } from 'src/app/components/sidebar/sidebar.module';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [SistemaComponent],
@@ -14,7 +17,11 @@ import { ReactiveFormsModule } from '@angular/forms';
     SistemaRoutingModule,
     NavbarModule,
     SidebarModule,
+    FormsModule,
     ReactiveFormsModule,
+    NgxPaginationModule,
+    NgSelectModule,
+    MatIconModule,
   ],
 })
 export class SistemaModule {}
